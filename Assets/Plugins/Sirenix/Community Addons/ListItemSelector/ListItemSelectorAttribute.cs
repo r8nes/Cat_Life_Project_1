060e559c472a0a96350c0b11191161ec2d7c99c6
boss.timysn94@outlook.com <boss.timysn94@outlook.com>
2023-03-31 20:58:46 +0300

    - Init, add Odin (only for pet project)

---
+++ Assets/Plugins/Sirenix/Community Addons/ListItemSelector/ListItemSelectorAttribute.cs
@@ -0,0 +1,13 @@
+﻿#if UNITY_EDITOR
+using System;
+
+public class ListItemSelectorAttribute : Attribute
+{
+    public string SetSelectedMethod;
+
+    public ListItemSelectorAttribute(string setSelectedMethod)
+    {
+        this.SetSelectedMethod = setSelectedMethod;
+    }
+}
+#endif
